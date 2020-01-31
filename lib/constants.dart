3608aba9ae8e302370ccc8d98191c5ec446d6d26
zenasgram <zenasgram@gmail.com>
2020-01-31 14:30:58 +0000

    human bend included

--- lib/constants.dart
+++ lib/constants.dart
@@ -44,67 +44,71 @@ Map<String, int> bendThresholdMap = {
   'Excellent': 3,
   'Amazing': 4,
   'Great': 5,
-  'Very Good': 6,
+  'Very good': 6,
   'Good': 7,
-  'Above Average': 8,
+  'Above average': 8,
   'Average': 9,
-  'Below Average': 10,
-  'Can Be Improved': 11,
+  'Below average': 10,
+  'Can be improved': 11,
   'Bad': 12,
   'Poor': 13,
-  'Very Poor': 14,
-  'Extremely Poor': 15,
+  'Very poor': 14,
+  'Extremely poor': 15,
   'Dangerous': 16,
-  'Very Dangerous': 17,
-  'Extremely Dangerous': 18,
+  'Very dangerous': 17,
+  'Extremely dangerous': 18,
   'Terrible': 19,
-  'Extremely Terrible': 20,
-  'Life Threatening': 21,
+  'Extremely terrible': 20,
+  'Life threatening': 21,
 };
 
 String getThreshold(int flexValue) {
-  if (flexValue < 100) {
-    return 'Fantastic';
-  } else if (flexValue < 200) {
-    return 'Super';
-  } else if (flexValue < 300) {
-    return 'Excellent';
-  } else if (flexValue < 400) {
-    return 'Amazing';
-  } else if (flexValue < 500) {
-    return 'Great';
-  } else if (flexValue < 600) {
-    return 'Very Good';
-  } else if (flexValue < 700) {
-    return 'Good';
-  } else if (flexValue < 800) {
-    return 'Above Average';
-  } else if (flexValue < 900) {
-    return 'Average';
-  } else if (flexValue < 1000) {
-    return 'Below Average';
-  } else if (flexValue < 1100) {
-    return 'Can Be Improved';
-  } else if (flexValue < 1200) {
-    return 'Bad';
-  } else if (flexValue < 1300) {
-    return 'Poor';
-  } else if (flexValue < 1400) {
-    return 'Very Poor';
-  } else if (flexValue < 1500) {
-    return 'Extremely Poor';
-  } else if (flexValue < 1600) {
-    return 'Dangerous';
-  } else if (flexValue < 1700) {
-    return 'Very Dangerous';
-  } else if (flexValue < 1800) {
-    return 'Extremely Dangerous';
-  } else if (flexValue < 1900) {
-    return 'Terrible';
-  } else if (flexValue < 2000) {
-    return 'Extremely Terrible';
-  } else {
-    return 'Life Threatening';
+  try {
+    if (flexValue < 100) {
+      return 'Fantastic';
+    } else if (flexValue < 200) {
+      return 'Super';
+    } else if (flexValue < 300) {
+      return 'Excellent';
+    } else if (flexValue < 400) {
+      return 'Amazing';
+    } else if (flexValue < 500) {
+      return 'Great';
+    } else if (flexValue < 600) {
+      return 'Very good';
+    } else if (flexValue < 700) {
+      return 'Good';
+    } else if (flexValue < 800) {
+      return 'Above average';
+    } else if (flexValue < 900) {
+      return 'Average';
+    } else if (flexValue < 1000) {
+      return 'Below average';
+    } else if (flexValue < 1100) {
+      return 'Can be improved';
+    } else if (flexValue < 1200) {
+      return 'Bad';
+    } else if (flexValue < 1300) {
+      return 'Poor';
+    } else if (flexValue < 1400) {
+      return 'Very poor';
+    } else if (flexValue < 1500) {
+      return 'Extremely poor';
+    } else if (flexValue < 1600) {
+      return 'Dangerous';
+    } else if (flexValue < 1700) {
+      return 'Very dangerous';
+    } else if (flexValue < 1800) {
+      return 'Extremely dangerous';
+    } else if (flexValue < 1900) {
+      return 'Terrible';
+    } else if (flexValue < 2000) {
+      return 'Extremely terrible';
+    } else {
+      return 'Life threatening';
+    }
+  } catch (e) {
+    print(e);
   }
 }
 
@@ -117,32 +121,32 @@ Map<String, String> adviceMap = {
   'Excellent': 'Keep it up and stay posture fit!',
   'Amazing': 'Keep it up and stay posture fit!',
   'Great': 'Keep it up and stay posture fit!',
-  'Very Good': 'Keep it up and stay posture fit!',
+  'Very good': 'Keep it up and stay posture fit!',
   'Good': 'Keep it up and stay posture fit!',
-  'Above Average': 'Keep it up and stay posture fit!',
+  'Above average': 'Keep it up and stay posture fit!',
   'Average': 'Can be improved, don\'t give up!',
-  'Below Average': 'Can be improved, don\'t give up!',
-  'Can Be Improved': 'Don\'t give up!',
+  'Below average': 'Can be improved, don\'t give up!',
+  'Can be improved': 'Don\'t give up!',
   'Bad': 'Can be improved, don\'t give up!',
   'Poor': 'Can be improved, don\'t give up!',
-  'Very Poor': 'Can be improved, don\'t give up!',
-  'Extremely Poor': 'Can be improved, don\'t give up!',
+  'Very poor': 'Can be improved, don\'t give up!',
+  'Extremely poor': 'Can be improved, don\'t give up!',
   'Dangerous': 'Can be improved, don\'t give up!',
-  'Very Dangerous': 'Can be improved, don\'t give up!',
-  'Extremely Dangerous': 'Can be improved, don\'t give up!',
+  'Very dangerous': 'Can be improved, don\'t give up!',
+  'Extremely dangerous': 'Can be improved, don\'t give up!',
   'Terrible': 'Can be improved, don\'t give up!',
-  'Extremely Terrible': 'Can be improved, don\'t give up!',
-  'Life Threatening': 'Can be improved, don\'t give up!',
+  'Extremely terrible': 'Can be improved, don\'t give up!',
+  'Life threatening': 'Can be improved, don\'t give up!',
 };
 
 void updateVariables(int xData, int yData, int timeThres) {
-  if (xData > timeThres) {
+  if (xData > timeThres && yData != null) {
     String statusKey = getThreshold(yData);
     selector = bendThresholdMap[statusKey];
 
     statusBend = statusKey;
     statusAdvice = adviceMap[statusKey];
 
-    print('Just updated bamboo!');
+//    print('Just updated bamboo!');
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,6 +12,8 @@ import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:syncfusion_flutter_core/core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import 'package:intl/intl.dart';
+
 class HomeScreen extends StatefulWidget {
   static String id = 'home_screen';
   @override
@@ -148,13 +150,32 @@ class _HomeScreenState extends State<HomeScreen>
                   thickness: 2,
                 ),
               ),
-              Flexible(
-                child: Container(
-                  alignment: Alignment(-0.15 + 0.3 * animation.value, 0),
-                  child: Image.asset('images/bamboo$selector.png'),
-//                  height: 300 + 10 * animation.value,
+              Expanded(
+                child: SizedBox(
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: <Widget>[
+                      Container(
+                        alignment: Alignment(1.2 + 0.1 * animation.value, 0),
+                        child: Image.asset('images/back$selector.png'),
+                        height: 200,
+                      ),
+                      Container(
+                        alignment: Alignment(-1.2 + -0.2 * animation.value, 0),
+                        child: Image.asset('images/front$selector.png'),
+                        height: 200,
+                      ),
+                    ],
+                  ),
                 ),
               ),
+//              Expanded(
+//                child: Container(
+//                  alignment: Alignment.center,
+//                  child: Image.asset('images/bamboo$selector.png'),
+//                  height: 200,
+//                ),
+//              ),
               Align(
                 alignment: FractionalOffset.bottomCenter,
                 child: Column(
@@ -177,34 +198,61 @@ class _HomeScreenState extends State<HomeScreen>
                                 .add(ChartData.fromMap(documentSnapshot.data));
                           }
                           widget = Container(
-                            height: 200,
-                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            height: 300,
+                            padding: EdgeInsets.only(right: 30),
                             child: SfCartesianChart(
                               primaryXAxis: DateTimeAxis(
-                                intervalType: DateTimeIntervalType.hours,
-                                interval: 2,
+                                dateFormat: DateFormat.jm(),
+                                intervalType: DateTimeIntervalType.minutes,
+                                interval: 10,
                                 maximum: DateTime.now(),
                                 minimum: DateTime.now()
-                                    .subtract(Duration(hours: 12)),
+                                    .subtract(Duration(minutes: 30)),
                               ),
                               primaryYAxis: NumericAxis(
-//                            maximum: 20000,
-//                            minimum: 0,
-                                  ),
+                                isVisible: true,
+                                maximum: 2200,
+                                minimum: 0,
+                                labelFormat: ' ',
+                                title: AxisTitle(
+                                    text: 'Slouch Level',
+                                    textStyle: ChartTextStyle(
+                                        color: Colors.white,
+                                        fontFamily: 'Montserrat',
+                                        fontSize: 15,
+                                        fontStyle: FontStyle.normal,
+                                        fontWeight: FontWeight.w600)),
+                              ),
                               series: <ChartSeries<ChartData, dynamic>>[
-                                SplineAreaSeries<ChartData, dynamic>(
+                                AreaSeries<ChartData, dynamic>(
                                     color: Colors.tealAccent,
+                                    gradient: LinearGradient(
+                                        begin: Alignment.topRight,
+                                        end: Alignment.bottomLeft,
+                                        stops: [
+                                          0.1,
+                                          0.4,
+                                          0.7,
+                                        ],
+                                        colors: [
+                                          Colors.teal[200],
+                                          Colors.orange[500],
+                                          Colors.red[500],
+                                        ]),
                                     opacity: 0.8,
                                     dataSource: chartData,
                                     xValueMapper: (ChartData data, _) {
-                                      updateVariables(
-                                          data.xValue
-                                              .toDate()
-                                              .millisecondsSinceEpoch,
-                                          data.yValue,
-                                          DateTime.now()
-                                              .subtract(Duration(minutes: 5))
-                                              .millisecondsSinceEpoch);
+                                      if (data.yValue != null) {
+                                        updateVariables(
+                                            data.xValue
+                                                .toDate()
+                                                .millisecondsSinceEpoch,
+                                            data.yValue,
+                                            DateTime.now()
+                                                .subtract(Duration(minutes: 60))
+                                                .millisecondsSinceEpoch);
+                                      }
+
                                       return data.xValue;
                                     },
                                     yValueMapper: (ChartData data, _) =>
@@ -212,7 +260,13 @@ class _HomeScreenState extends State<HomeScreen>
                               ],
                               margin: EdgeInsets.all(20),
                               plotAreaBorderColor: Colors.transparent,
-                              tooltipBehavior: TooltipBehavior(enable: true),
+                              tooltipBehavior: TooltipBehavior(
+                                enable: true,
+                                borderColor: Colors.tealAccent,
+                                borderWidth: 1,
+                                color: Colors.teal,
+                                activationMode: ActivationMode.singleTap,
+                              ),
                             ),
                           );
                         }
@@ -220,8 +274,8 @@ class _HomeScreenState extends State<HomeScreen>
                       },
                     ),
                     Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 15),
+                      padding: const EdgeInsets.only(
+                          top: 15, left: 15, right: 15, bottom: 0),
                       child: Card(
                         shape: RoundedRectangleBorder(
                           side: BorderSide(color: Colors.white70, width: 1),
@@ -259,8 +313,8 @@ class _HomeScreenState extends State<HomeScreen>
                                   color: Colors.black,
                                   textStyle:
                                       Theme.of(context).textTheme.display1,
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.w200,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w300,
                                   fontStyle: FontStyle.normal,
                                 ),
                               ),
